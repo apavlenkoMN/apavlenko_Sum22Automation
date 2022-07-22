4b1c752734136e699620a5b965fad0a4d18f6ba0
Aleksey Pavlenko <apavlenko@modeln.com>
2022-07-22 22:05:39 +0300

    HomeTask6 part2, Product class, list class with sorting by and printing

---
+++ java_core_lessons/src/collections/BuyList.java
@@ -0,0 +1,51 @@
+package collections;
+
+import java.text.Collator;
+import java.util.*;
+
+
+public class BuyList {
+
+
+//    Создать программу - список покупок. В этот список заносятся объекты типа Product,
+//    у которого есть поля name, quantity, price.
+//    Написать программу, которая выводит полный список покупок на экран
+//    (название - количество - общая стоимость).
+//    Вывести список 2 раза: отсортированный по названию продуктов, и по общей стоимости.
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        Product product1 = new Product("Prod1", random.nextInt(100), 23);
+        Product product2 = new Product("Prod2", random.nextInt(100), 63.41);
+        Product product3 = new Product("Prod3", random.nextInt(100), 13.05);
+
+
+        List<Product> buyList = new LinkedList<>(Arrays.asList(new Product("Product5", 3, 22.12), new Product("Product6", 23, 12.12), new  Product("Product7", 5, 42.12), new Product("Product2", 1, 2.12)));
+
+        buyList.add(product1);
+        buyList.add(product2);
+        buyList.add(product3);
+
+        System.out.println("original: " + buyList);
+
+        Collections.sort(buyList, new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o2.getTotalCost() - o1.getTotalCost();
+            }
+        });
+
+        System.out.println("sort by Total Cost: " + buyList);
+
+
+        Collections.sort(buyList, Comparator.comparing(obj -> obj.getName()));
+
+        System.out.println("sort by name: " + buyList);
+
+
+    }
+
+
+}
---
+++ java_core_lessons/src/collections/Product.java
@@ -0,0 +1,71 @@
+package collections;
+
+public class Product {
+
+    private int quantity;
+    private String name;
+    private double price;
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getTotalCost(){
+        return this.quantity * (int)this.price;
+    }
+
+
+    public Product(String name, int quantity, double price){
+        setName(name);
+        setPrice(price);
+        setQuantity(quantity);
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Product {" +
+//                "quantity=" + quantity +
+//                ", name='" + name + '\'' +
+//                ", price=" + price +
+//                '}';
+//    }
+
+    @Override
+    public String toString() {
+        return "{" + name  +
+                " , quantity = " + quantity +
+                ", price = " + price +
+                '}';
+    }
+
+    public void printProduct(Product p){
+        System.out.print(p.getName() + " - ");
+        System.out.print(p.getQuantity() + " - ");
+        System.out.print(p.getPrice());
+        System.out.println();
+    }
+
+
+
+}
