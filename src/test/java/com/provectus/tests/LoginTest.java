0566702d85501b064a9ceb5697f0b6aad716ad12
Aleksey Pavlenko <apavlenko@modeln.com>
2022-08-01 19:35:12 +0300

    main test

---
+++ src/test/java/com/provectus/tests/LoginTest.java
@@ -0,0 +1,153 @@
+package com.provectus.tests;
+
+import Pages.BaseLogin;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest extends BaseTest{
+
+    @BeforeTest
+    public void before() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void tearDown()
+    {
+        driver.close();
+        driver.quit();
+    }
+
+
+
+    @DataProvider(name = "userProvider")
+    public Object[][] userProvider(){
+        Object[][] result = {
+                {"user1", "pass1"},
+//                {"user2", "pass2"},
+                {"tomsmith", "pass3"}
+        };
+        return result;
+    }
+
+    private static WebDriver driver;
+
+    @Test(groups = {"Group1"}, dataProvider = "userProvider")
+    public void LoginTest1(String user, String passw) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        WebElement username = driver.findElement(By.id("username"));
+        //username.sendKeys("tomsmith");
+        username.sendKeys(user);
+
+//        WebElement username2 = userNameInput;
+//        username2.sendKeys("tomsmith2");
+
+//        userNameInput.sendKeys("tomsmith3");
+
+        if (user == "tomsmith") {
+            WebElement pass = driver.findElement(By.id("password"));
+            pass.sendKeys("SuperSecretPassword!");
+        } else {
+            WebElement pass = driver.findElement(By.id("password"));
+//        pass.sendKeys("SuperSecretPassword!2");
+            pass.sendKeys(passw);
+        }
+
+        WebElement submit = driver.findElement(By.cssSelector("button.radius"));
+        submit.click();
+
+        WebElement header = driver.findElement(By.tagName("h2"));
+        Assert.assertEquals(header.getText(), "Secure Area");
+
+        // Assert.assertEquals(getHeaderText(),"Secure Area");
+
+        driver.quit();
+    }
+    @Test (groups = {"Group2"}, dependsOnGroups = {"Group1"}, description = "depends on test1")
+    public void LoginTest2() throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+        WebElement pass = driver.findElement(By.id("password"));
+        pass.sendKeys("SuperSecretPassword!");
+
+        WebElement submit = driver.findElement(By.cssSelector("button.radius"));
+        submit.click();
+
+        WebElement header = driver.findElement(By.tagName("h2"));
+        Assert.assertEquals(header.getText(),"Secure Area");
+
+        // Assert.assertEquals(getHeaderText(),"Secure Area");
+
+        driver.quit();
+    }
+
+
+    //
+//    @Test (groups = "Group1", description = "depends on test1")
+//    public void LoginTest2() {
+//
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.get("https://the-internet.herokuapp.com/login");
+//
+//        userNameInput.sendKeys("tomsmith");
+//        password.sendKeys("SuperSecretPassword!");
+//        submit.click();
+//
+//        Assert.assertEquals(getHeaderText(),"Secure Area");
+//
+//        driver.quit();
+//    }
+//
+//
+    @Parameters({"name", "pass"})
+    @Test (groups = {"Group2"})
+    public void LoginTest3(String name, String password) {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys(name);
+
+        WebElement pass = driver.findElement(By.id("password"));
+        pass.sendKeys(password);
+        //pass.sendKeys(System.getProperty("pass"));
+
+        WebElement submit = driver.findElement(By.cssSelector("button.radius"));
+        submit.click();
+
+        WebElement header = driver.findElement(By.tagName("h2"));
+
+        Assert.assertEquals(header.getText(),"Login Page");
+
+        driver.quit();
+    }
+
+
+
+
+}
