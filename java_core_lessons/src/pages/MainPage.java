55f13a4b0b33f9d5d58e5854573dd66592087b2e
Aleksey Pavlenko <apavlenko@modeln.com>
2022-08-10 16:22:41 +0300

    add mainPage

---
+++ java_core_lessons/src/pages/MainPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPage extends BasePage{
+
+
+
+    public MainPage(WebDriver driver){
+        super(driver); //вызвать родительский конструктор
+
+    }
+
+    public LoginPage goToAuthPage(){
+        driver.findElement(By.linkText("Form Authentication")).click();
+        return new LoginPage(driver);
+    }
+
+
+   // https://the-internet.herokuapp.com/
+
+
+}
