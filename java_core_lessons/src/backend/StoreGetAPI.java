73072e7c143ba3554cd7b2bef9401569af5b098a
Aleksey Pavlenko <apavlenko@modeln.com>
2022-08-31 15:18:27 +0300

    update format

--- java_core_lessons/src/backend/StoreGetAPI.java
+++ java_core_lessons/src/backend/StoreGetAPI.java
@@ -13,58 +13,6 @@ import java.io.IOException;
 
 public class StoreGetAPI {
 
-    //public static void main(String[] args) throws IOException {
-
-
-    //public JSONObject responseJson; // = new JSONObject();
-
-//    public String createOrder  (String name) throws IOException{
-//
-//
-//        JSONObject json = new JSONObject();
-//        json.put("id", "1");
-//        json.put("petId", "1");
-//        json.put("quantity", "2");
-//        json.put("shipDate", "2022-08-24T12:29:36.909+0000");
-//        json.put("status", "placed");
-//        json.put("complete", " true");
-//
-//
-//        System.out.println("json to string");
-//        System.out.println(json.toString());
-//
-//        RequestBody body = RequestBody.create(json.toString().getBytes());
-//        //  RequestBody body = RequestBody.create(MediaType.parse("application/json"), json);
-//
-//        //формирует запрос
-//        Request request = new Request.Builder()
-//                .post(body)
-//                .url("https://petstore.swagger.io/v2/store/order")
-//                .addHeader("Content-Type", "application/json")
-//                .build();
-//
-//
-//
-//        OkHttpClient client = new OkHttpClient();
-//        //отправляет запрос
-//        Response response = client.newCall(request).execute();
-//
-//        System.out.println(response.code());
-//        //System.out.println(response.body().string());
-//
-//        System.out.println("set response body");
-//
-//        String responseBody = response.body().string();
-//        //JSONObject responseJson = new JSONObject(responseBody); //передаем строку в объект
-//        JSONObject responseJson = new JSONObject(responseBody);
-//        System.out.println("response get id");
-//        return responseJson.get("id").toString();
-//
-//    }
-
-
-
-
     public void getOrderById (Integer id) throws IOException{
 
 
